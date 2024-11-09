dcab92bc674ef6c22fd1008ba671eb01e0e8de3d
tacheraSasi <tacherasasi@gmail.com>
2024-11-09 23:13:26 +0300

    refactored the code in main.go and made it work for both listing and counting lines

--- main.go
+++ main.go
@@ -9,52 +9,30 @@ import (
 )
 
 func main() {
+	
 	// Getting the current directory or take from args
 	dir := "."
 	if len(os.Args) > 1 {
-		if os.Args[1] == "count-lines"{
-			countLines(os.Args[2])
-			return
-		}
 		dir = os.Args[1]
 	}
-
-	// Opening the directory
-	directory, err := os.Open(dir)
-	if err != nil {
-		fmt.Println("Error opening directory:", err)
-		return
+	switch os.Args[1] {
+	case "list":
+		listFiles(dir)
+	case "count line":
+		countLines(os.Args[2])
+	default:
+		log.Fatal("please enter a valid option")
+		
 	}
-	defer directory.Close()
+	
 
-	// Reading the directory contents
-	files, err := directory.Readdir(-1) // -1 means read all entries
-	if err != nil {
-		fmt.Println("Error reading directory:", err)
-		return
-	}
-
-	// Looping through the files and display information
-	fmt.Println("Listing files in directory:", dir)
-	for _, file := range files {
-		// File name
-		fmt.Printf("%-25s", file.Name())
-
-		// File size
-		fmt.Printf("%-10d", file.Size())
-
-		// File permissions
-		fmt.Printf("%-10s", file.Mode().String())
-
-		// Last modified time
-		modTime := file.ModTime().Format(time.RFC1123)
-		fmt.Printf("%-20s\n", modTime)
-	}
+	
 }
 
 func countLines(filePath string){
 	if filePath == "" {
-		filePath = "."
+		log.Fatal("Please specify a file")
+		return
 	}
 	file,err := os.Open(filePath) 
 	if err != nil{
@@ -78,5 +56,40 @@ func countLines(filePath string){
 		return
 	 }
 	 fmt.Println("Number of lines in file:", count)
+
+}
+
+func listFiles(dirPath string){
+	// Opening the directory
+	directory, err := os.Open(dirPath)
+	if err != nil {
+		fmt.Println("Error opening directory:", err)
+		return
+	}
+	defer directory.Close()
+
+	// Reading the directory contents
+	files, err := directory.Readdir(-1) // -1 means read all entries
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	// Looping through the files and display information
+	fmt.Println("Listing files in directory:", dirPath)
+	for _, file := range files {
+		// File name
+		fmt.Printf("%-25s", file.Name())
+
+		// File size
+		fmt.Printf("%-10d", file.Size())
+
+		// File permissions
+		fmt.Printf("%-10s", file.Mode().String())
+
+		// Last modified time
+		modTime := file.ModTime().Format(time.RFC1123)
+		fmt.Printf("%-20s\n", modTime)
+	}
 
 }
